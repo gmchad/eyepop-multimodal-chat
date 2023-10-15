7962718f087ee1f8507367a58bbac2a8052ae724
chadlohrli <chad.david.lohrli@gmail.com>
2023-10-14 22:41:20 -0700

    integrate eyepop

--- chatbot.py
+++ chatbot.py
@@ -2,22 +2,112 @@ import os
 import openai
 import gradio as gr
 from dotenv import load_dotenv
+import requests
+import json
+
 load_dotenv()
 
 OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
+POP_ENDPOINT = os.getenv('POP_ENDPOINT')
+TOKEN = os.getenv('TOKEN')
+
 openai.api_key = OPEN_AI_KEY
+CONFIG = None
+
+prompt = """
+You have been connected to a convolutional neural network(CNN). \
+The problem is that the CNN can only give you JSON information about \
+the picture that it is looking at. The keys are what the CNN identified \
+in the image, and the values indicate how much of it was detected. \
+Your job is to come up with assumptions of what you think the picture \
+is and what is happening within the picture
+"""
+
+# file name to image description
+image_dict = {}
+
+def fetch_pop_config(pop_endpoint, token):
+    headers = {"Accept": "application/json", "Authorization": f"Bearer {token}"}
+    response = requests.get(pop_endpoint, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise("Error fetching config")
+
+
+def sort_and_count(labels):
+    """
+    Takes a list of labels and returns a sorted dictionary
+    where the keys are the labels and the values are the counts of each label.
+    """
+    count_dict = {}
+    for label in labels:
+        if label in count_dict:
+            count_dict[label] += 1
+        else:
+            count_dict[label] = 1
+
+    # Sort the dictionary by label
+    sorted_dict = dict(sorted(count_dict.items()))
+    return sorted_dict
+
+def get_json_from_eyepop_file(file_path, config):
+    # Use a with statement to ensure the file is closed after its suite finishes
+    with open(file_path, "rb") as f:
+        # Create a dictionary to hold the file data
+        files = {"file": f}
+
+        url = f"{config['url']}/pipelines/{config['pipeline_id']}/source?mode=preempt&processing=sync"
+        headers = {"accept": "application/json"}
+
+        response = requests.post(url, headers=headers, files=files)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            classy_labels = [obj["classLabel"] for obj in data[0]["objects"]]
+            sorted_objects = sort_and_count(classy_labels)
+            output_string = json.dumps(sorted_objects)
+            print(output_string)
+            return output_string
+        else:
+            raise("The party's over, there was an error:", response.content)
+
+def add_file(history, file):
+  history = history + [[[file.name,], None]]
+  # get image description from eyepop
+  image_file = get_json_from_eyepop_file(file.name, CONFIG)
+  # map file name to eyepop description
+  image_dict[file.name] = image_file
+  return history
 
 def user(user_message, history):
   return history + [[user_message, None]]
 
 def predict(message, history):
+    print(1,history)
     history_openai_format = []
+    image_file = None
     for human, assistant in history:
-        # this is always None for the latest message
+        # gradio likes tuples for images, gpt does not, so we need to check
+        if isinstance(human, tuple):
+          image_file = human[0]
+          human = human[0]
+          print(image_file)
+        # if assistant is not None, then we have a response from gpt
         if assistant:
           history_openai_format.append({"role": "user", "content": human })
           history_openai_format.append({"role": "assistant", "content":assistant})
+      
+    # push image description on the stack    
+    if image_file:
+      # get image description from dictionary and inject prompt 
+      image_description = image_dict[image_file]
+      message = prompt + "\n" + image_description
+      
     history_openai_format.append({"role": "user", "content": message})
+    print(2, history_openai_format)
   
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
@@ -34,8 +124,21 @@ def predict(message, history):
 
 with gr.Blocks() as demo:
   chatbot = gr.Chatbot()
-  msg = gr.Textbox()
-  msg.submit(user, [msg, chatbot], [chatbot], queue=False).then( 
-    predict, [msg, chatbot], chatbot)
   
-demo.queue().launch()
+  with gr.Row():
+    msg = gr.Textbox(
+      scale=4,
+      container=False
+    )
+    btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
+    
+    file_msg = btn.upload(add_file, [chatbot, btn], [chatbot], queue=False).then(
+        predict, [msg, chatbot], chatbot
+    )
+    
+    msg.submit(user, [msg, chatbot], [chatbot], queue=False).then( 
+      predict, [msg, chatbot], chatbot)
+  
+if __name__ == "__main__":
+  CONFIG = fetch_pop_config(POP_ENDPOINT, TOKEN)
+  demo.queue().launch()
