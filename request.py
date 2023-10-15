657d32d0e9b944c18994d2c6862646ccb710c08c
Namdar Mesri <namdar@MacBook-Pro-2.lan>
2023-10-14 22:01:19 -0700

    fix output string

--- request.py
+++ request.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
 
 load_dotenv()
 
@@ -18,6 +19,25 @@ def fetch_pop_config(pop_endpoint, token):
         return None
 
 
+def sort_and_count(labels):
+    """
+    Takes a list of labels and returns a sorted dictionary
+    where the keys are the labels and the values are the counts of each label.
+    """
+    count_dict = {}
+
+    for label in labels:
+        if label in count_dict:
+            count_dict[label] += 1
+        else:
+            count_dict[label] = 1
+
+    # Sort the dictionary by label
+    sorted_dict = dict(sorted(count_dict.items()))
+
+    return sorted_dict
+
+
 def get_json_from_eyepop_file(file_path, config):
     # Use a with statement to ensure the file is closed after its suite finishes
     with open(file_path, "rb") as f:
@@ -32,8 +52,15 @@ def get_json_from_eyepop_file(file_path, config):
         # Check if the request was successful
         if response.status_code == 200:
             data = response.json()
-            print("Success:", data)
-
+            classy_labels = [obj["classLabel"] for obj in data[0]["objects"]]
+            sorted_objects = sort_and_count(classy_labels)
+            print(sorted_objects)
+            output_string = "\n".join(
+                f"{key}: {value}" for key, value in sorted_objects.items()
+            )
+            # Write the string to a file
+            with open("output.txt", "w") as file:
+                file.write(output_string)
         else:
             print("The party's over, there was an error:", response.content)
 
